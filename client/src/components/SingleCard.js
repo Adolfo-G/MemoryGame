6908c911a1deeed6ae80ab2ebed891128632f909
Adolfo Gonzalez <agonzal177@gmail.com>
2022-10-19 16:38:24 -0400

    changed card layout and added card removal

--- client/src/components/SingleCard.js
+++ client/src/components/SingleCard.js
@@ -1,25 +1,27 @@
 import './SingleCard.css';
 
 export default function SingleCard({ card, handleChoice }) {
-
   const handleClick = () => {
     handleChoice(card)
+    
   }
-
-    return (
-      <div className= "card">
-        <div>
-          <img 
-            className="front" 
-            src="images/cards/coverCard.png"
-            alt="card front"/>
-          <img 
-            className="back" 
-            src={card.src} 
-            onClick={handleClick} 
-            alt="card back" 
-          />
-        </div>
-      </div>
-    )
+  return (
+    <>
+      {card.src ?
+        <div className="card" >
+            {/* <img
+              className="front"
+              src="images/cards/coverCard.png"
+              alt="card front" /> */}
+            <img
+              className="back"
+              src={card.src}
+              onClick={handleClick}
+              alt="card back"
+            />
+        </div> :
+        <div className='card'></div>
+      }
+    </>
+  )
 }
