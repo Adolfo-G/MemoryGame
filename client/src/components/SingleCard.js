1d90d4245a5677eed01615ad7fcd35724e21d553
Amy Shafer <amy.u.shafer@gmail.com>
2022-10-22 19:34:14 -0400

    git troubleshooting

--- client/src/components/SingleCard.js
+++ client/src/components/SingleCard.js
@@ -1,27 +1,27 @@
 import './SingleCard.css';
 
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+
   const handleClick = () => {
-    handleChoice(card)
-    
+    if (!disabled) {
+      handleChoice(card); 
+    }
   }
-  return (
-    <>
-      {card.src ?
-        <div className="card" >
-            {/* <img
-              className="front"
-              src="images/cards/coverCard.png"
-              alt="card front" /> */}
-            <img
-              className="back"
-              src={card.src}
-              onClick={handleClick}
-              alt="card back"
-            />
-        </div> :
-        <div className='card'></div>
-      }
-    </>
-  )
+
+    return (
+      <div className= "card">
+        <div className={flipped ? "flipped" : ""}>
+          <img 
+            className="front" 
+            src={card.src} 
+            alt="card front"/>
+          <img 
+            className="back" 
+            src="images/cards/coverCard.png"
+            onClick={handleClick} 
+            alt="card back" 
+          />
+        </div>
+      </div>
+    )
 }
